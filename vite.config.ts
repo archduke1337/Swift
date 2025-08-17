32c7c96165744ab8ac18e26044bf9b364cbac6aa
archduke1337 <198441721+archduke1337@users.noreply.github.com>
2025-08-17 13:13:20 +0530

    main

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(process.cwd(), 'client', 'src'),
       '@shared': path.resolve(process.cwd(), 'shared'),
-      '@assets': path.resolve(process.cwd(), 'attached_assets'),
+      '@assets': path.resolve(process.cwd(), 'client', 'public', 'assets'),
     },
   },
   root: path.resolve(process.cwd(), 'client'),
